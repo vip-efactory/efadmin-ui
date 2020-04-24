7fdc8d5d5738966aa49e03689c08437855d7d943
dbdu <1797890817@qq.com>
2020-04-24 19:56:28 +0800

    优化员工组件

--- src/views/system/employee/index.vue
+++ src/views/system/employee/index.vue
@@ -13,7 +13,7 @@
       <!--如果想在工具栏加入更多按钮，可以使用插槽方式， slot = 'left' or 'right'-->
       <crudOperation :permission="permission" />
       <!--表单组件-->
-      <el-dialog :close-on-click-modal="false" :before-close="crud.cancelCU" :visible.sync="crud.status.cu > 0" :title="crud.status.title" width="520px">
+      <el-dialog v-dialogDrag :close-on-click-modal="false" :before-close="crud.cancelCU" :visible.sync="crud.status.cu > 0" :title="crud.status.title" width="520px">
         <el-form ref="form" :model="form" :rules="rules" size="small" label-width="100px">
           <el-form-item :label="$t('employee.name')" prop="name">
             <el-input v-model="form.name" style="width: 370px;" />
@@ -40,13 +40,32 @@
             <el-input v-model="form.phone" style="width: 370px;" />
           </el-form-item>
           <el-form-item :label="$t('employee.status')">
-            <el-input v-model="form.status" style="width: 370px;" />
+            <el-radio-group v-model="form.status" >
+              <el-radio
+                v-for="item in dict.employee_status"
+                :key="item.id"
+                :label="item.value"
+              >{{ item.label }}</el-radio>
+            </el-radio-group>
           </el-form-item>
-          <el-form-item :label="$t('employee.dept')">
-            <el-input v-model="form.deptId" style="width: 370px;" />
+          <el-form-item :label="$t('employee.dept')" prop="dept.id">
+            <treeselect
+              v-model="form.dept.id"
+              :options="depts"
+              style="width: 178px"
+              :placeholder="$t('user.selectDept')"
+              @select="selectFun"
+            />
           </el-form-item>
-          <el-form-item :label="$t('employee.job')">
-            <el-input v-model="form.jobId" style="width: 370px;" />
+          <el-form-item :label="$t('employee.job')" prop="job.id">
+            <el-select v-model="form.job.id" style="width: 178px" :placeholder="$t('user.selectJob')" :title="$t('user.selectJob')">
+              <el-option
+                v-for="(item, index) in jobs"
+                :key="item.name + index"
+                :label="item.name"
+                :value="item.id"
+              />
+            </el-select>
           </el-form-item>
           <el-form-item :label="$t('be.remark')">
             <el-input v-model="form.remark" :rows="3" type="textarea" style="width: 370px;" />
@@ -61,7 +80,7 @@
       <el-table ref="table" v-loading="crud.loading" :data="crud.data" size="small" style="width: 100%;" @selection-change="crud.selectionChangeHandler" @sort-change="crud.doTitleOrder">
         <el-table-column type="selection" width="55" />
         <el-table-column v-if="columns.visible('name')" prop="name" :label="$t('employee.name')" sortable="custom" />
-        <el-table-column v-if="columns.visible('address')" prop="address" :label="$t('employee.address')" sortable="custom" />
+        <el-table-column v-if="columns.visible('address')" :show-overflow-tooltip="true" prop="address" :label="$t('employee.address')" sortable="custom" />
         <el-table-column v-if="columns.visible('avatar')" prop="avatar" :label="$t('employee.avatar')" />
         <el-table-column v-if="columns.visible('birthday')" prop="birthday" :label="$t('employee.birthday')" sortable="custom" />
         <el-table-column v-if="columns.visible('code')" prop="code" :label="$t('employee.code')" sortable="custom" />
@@ -70,11 +89,15 @@
         <el-table-column v-if="columns.visible('phone')" prop="phone" :label="$t('employee.phone')" sortable="custom" />
         <el-table-column v-if="columns.visible('status')" prop="status" :label="$t('employee.status')" sortable="custom">
           <template slot-scope="scope">
-            {{ dict.label.user_status[scope.row.status] }}
+            {{ dict.label.employee_status[scope.row.status] }}
           </template>
         </el-table-column>
-        <el-table-column v-if="columns.visible('deptId')" prop="deptId" :label="$t('employee.dept')" sortable="custom" />
-        <el-table-column v-if="columns.visible('jobId')" prop="jobId" :label="$t('employee.job')" sortable="custom" />
+<!--        <el-table-column v-if="columns.visible('dept')" prop="dept" :label="$t('employee.dept')" sortable="custom" />-->
+        <el-table-column v-if="columns.visible('job')" prop="job" :label="$t('employee.job')" sortable="custom" >
+          <template slot-scope="scope">
+            <div>{{ scope.row.dept.name }} / {{ scope.row.job.name }}</div>
+          </template>
+        </el-table-column>
         <el-table-column v-if="columns.visible('remark')" prop="remark" :label="$t('be.remark')" sortable="custom" />
         <el-table-column v-if="columns.visible('createTime')" prop="createTime" :label="$t('be.createTime')" sortable="custom">
           <template slot-scope="scope">
@@ -104,25 +127,30 @@
 </template>
 
 <script>
+import { getDepts } from '@/api/system/dept'
+import { getAllJob } from '@/api/system/job'
 import crudEmployee from '@/api/system/employee'
 import CRUD, { presenter, header, form, crud } from '@crud/crud'
 import rrOperation from '@crud/RR.operation'
 import crudOperation from '@crud/CRUD.operation'
 import udOperation from '@crud/UD.operation'
 import pagination from '@crud/Pagination'
+import Treeselect from '@riophae/vue-treeselect'
+import '@riophae/vue-treeselect/dist/vue-treeselect.css'
 import i18n from '../../../lang'
 
 // crud交由presenter持有
 const adSearchFields = new Map([['name', i18n.t('employee.name')], ['phone', i18n.t('employee.phone')], ['createTime', i18n.t('be.createTime')], ['updateTime', i18n.t('be.updateTime')], ['remark', i18n.t('be.updateTime')]]) // 需要高级搜索的字段
 const defaultCrud = CRUD({ title: i18n.t('employee.TITLE'), url: 'api/employee/page', crudMethod: { ...crudEmployee }, adSearchFields: adSearchFields })
-const defaultForm = { id: null, address: null, avatar: null, birthday: null, code: null, email: null, idNumber: null, name: null, phone: null, status: null, deptId: null, jobId: null, remark: null, createTime: null, creatorNum: null, updateTime: null, updaterNum: null }
+const defaultForm = { id: null, address: null, avatar: null, birthday: null, code: null, email: null, idNumber: null, name: null, phone: null, status: null, dept: { id: null }, job: { id: null }, remark: null, createTime: null, creatorNum: null, updateTime: null, updaterNum: null }
 export default {
   name: 'Employee',
-  components: { pagination, crudOperation, rrOperation, udOperation },
+  components: { Treeselect, pagination, crudOperation, rrOperation, udOperation },
   mixins: [presenter(defaultCrud), header(), form(defaultForm), crud()],
-  dicts: ['user_status'],
+  dicts: ['employee_status'],
   data() {
     return {
+      deptName: '', depts: [], jobs: [],
       permission: {
         add: ['admin', 'employee:add'],
         edit: ['admin', 'employee:edit'],
@@ -139,6 +167,17 @@ export default {
     }
   },
   methods: {
+
+    // 新增与编辑前做的操作
+    [CRUD.HOOK.afterToCU](crud, form) {
+      this.getDepts()
+    },
+    // 打开编辑弹窗前做的操作
+    [CRUD.HOOK.beforeToEdit](crud, form) {
+      this.getJobs(this.form.dept.id)
+      form.status = form.status.toString()
+    },
+
     // 获取数据前设置好接口地址
     [CRUD.HOOK.beforeRefresh]() {
       const query = this.query
@@ -146,6 +185,32 @@ export default {
         this.crud.params[query.type] = query.value
       }
       return true
+    },
+    // 获取弹窗内部门数据
+    getDepts() {
+      getDepts({ enabled: true }).then(res => {
+        if (res.code === 0) {
+          this.depts = res.data.content
+        } else {
+          this.crud.notify(res.msg, CRUD.NOTIFICATION_TYPE.ERROR)
+        }
+      })
+    },
+
+    // 获取弹窗内岗位数据
+    getJobs(id) {
+      getAllJob(id).then(res => {
+        if (res.code === 0) {
+          this.jobs = res.data.content
+        } else {
+          this.crud.notify(res.msg, CRUD.NOTIFICATION_TYPE.ERROR)
+        }
+      }).catch(() => { })
+    },
+    // 点击部门搜索对应的岗位
+    selectFun(node, instanceId) {
+      this.getJobs(node.id)
+      this.form.job.id = null
     }
   }
 }
