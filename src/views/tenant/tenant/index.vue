bb57acc3c191bb0ff8a97cdc8471268a007bf0cf
lyuzhang <3388545504@qq.com>
2026-01-13 09:56:48 +0800

    修改：修复了系统租户管理搜索功能

--- src/views/tenant/tenant/index.vue
+++ src/views/tenant/tenant/index.vue
@@ -4,8 +4,8 @@
     <div class="head-container">
       <div v-if="crud.props.searchToggle">
         <!-- 搜索 -->
-        <el-input v-model="query.value" clearable placeholder="输入搜索内容" style="width: 200px;" class="filter-item" @keyup.enter="crud.toQuery" />
-        <el-select v-model="query.type" clearable placeholder="类型" class="filter-item" style="width: 130px">
+        <el-input v-model="crud.query.value" clearable placeholder="输入搜索内容" style="width: 200px;" class="filter-item" @keyup.enter="crud.toQuery" />
+        <el-select v-model="crud.query.type" clearable placeholder="类型" class="filter-item" style="width: 130px">
           <el-option v-for="item in queryTypeOptions" :key="item.key" :label="item.display_name" :value="item.key" />
         </el-select>
         <rrOperation :crud="crud" />
@@ -162,7 +162,7 @@ export default {
   methods: {
     // 获取数据前设置好接口地址
     [CRUD.HOOK.beforeRefresh]() {
-      const query = this.query
+      const query = this.crud.query
       if (query.type && query.value) {
         this.crud.params[query.type] = query.value
       }
